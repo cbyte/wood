95bf433b2fef85471c989f7f140e043724e0ddfa
simonsch <simon-s@online.de>
2013-04-26 00:32:27 +0200

    Added default font image

--- base.js
+++ base.js
@@ -95,6 +95,12 @@ function max(a,b) {
 	return a>b?a:b;
 }
 
+function rand(min,max) {
+	if(min>max) return rand(max,min);
+	if(min==max) return min;
+	return min+parseInt(Math.random()*(max-min+1));
+}
+
 function Init() {
 	Renderer.initGL();
 	Renderer.initShader();
