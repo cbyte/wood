6c4a3acb9bede9dc43ac550c06daa242329fe06b
cbyte <simon-s@online.de>
2015-10-18 17:26:58 +0200

    Add more graphic classes

---
+++ src/graphics/material.js
@@ -0,0 +1,13 @@
+function Material() {
+	return {
+		texture: "default.png",
+		shader: "default",
+		ambient: [0,0,0],
+		diffuse: [1,1,1],
+		specular: [0,0,0],
+		emmissive: [0,0,0],
+		shininess: [0,0,0],
+		opacity: 1,
+		refraction: 1
+	}
+}
