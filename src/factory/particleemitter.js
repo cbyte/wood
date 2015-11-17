3534b78af04117b300d240354fd4d2cbaf65f38e
cbyte <admin@chocobyte.com>
2015-11-17 21:24:23 +0100

    Add broker server dependencies to gitignore and add factory classes

---
+++ src/factory/particleemitter.js
@@ -0,0 +1,12 @@
+function ParticleEmitter(pos, template) {
+  this.objects = 0;
+
+  this.rate = 0;
+  this.velocity = vec3.create();
+  this.decay = vec3.create();
+  this.rand = 0;
+
+  this.update = function() {
+
+  }
+}
